38f173c7ad9a23356f0bfdd4a552a944a01d9a2a
Logan Hasson <logan.hasson@gmail.com>
2014-01-14 12:18:02 -0500

    Complete solution

--- ruby_phone_format.rb
+++ ruby_phone_format.rb
@@ -1,1 +1,9 @@
 # Write your code here
+def normalize_phone_number(phone_number)
+  clean_number = phone_number.gsub(/\D/, '')
+  if clean_number.length != 10
+    phone_number
+  else
+    "(#{clean_number[0,3]}) #{clean_number[3,3]}-#{clean_number[6,4]}"
+  end
+end
