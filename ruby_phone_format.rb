ca486cefb3b6577d067687e8816cfdd8503719fa
gjc2118 <gjc2118@gmail.com>
2015-05-30 11:03:23 -0700

    cool

--- ruby_phone_format.rb
+++ ruby_phone_format.rb
@@ -1,1 +1,14 @@
 # Write your code here
+def normalize_phone_number(number)
+  number = number.to_s
+  copy = number.clone
+  number.gsub!(/\D/,"")
+  returnnumber = ""
+  if number.length ==10
+    array = number.split("")
+    returnnumber << "("+array[0]+array[1]+array[2]+") "+array[3]+array[4]+array[5]+"-"+array[6]+array[7]+array[8]+array[9]
+  else
+    copy
+  end
+end
+
