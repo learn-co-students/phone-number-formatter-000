896c304c67d70301298a77536c56205639322796
aeremin <aeremin@maxus.ru>
2015-06-18 18:34:43 +0300

    done

--- ruby_phone_format.rb
+++ ruby_phone_format.rb
@@ -1,1 +1,5 @@
 # Write your code here
+def normalize_phone_number(phone_number)
+  ph_n = phone_number.gsub(/[\s\-\(\)]/,'')
+  ph_n[/\d{10}/] == nil ? phone_number : "(#{ph_n[0..2]}) #{ph_n[3..5]}-#{ph_n[6..9]}" 
+end
